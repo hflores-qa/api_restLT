d4de8526012d30cd3ea4d2c9c23d6224f11ecdf7
sgsanmigo33-star <sgsanmigo33@gmail.com>
2026-03-14 10:48:28 -0500

    Add test for fetching user by ID endpoint
    Added a test to validate the endpoint for fetching a user by ID.

--- src/test/java/tests/SandraTest.java
+++ src/test/java/tests/SandraTest.java
@@ -3,5 +3,23 @@ package tests;
 public class SandraTest extends BaseTest {
     public void test1() {
 
+@Test
+    @Order(2)
+    @Tag("USER")
+    @Story("Obtener usuario por ID")
+    @Description("Validar que el endpoint /posts/{id} devuelve el usuario correcto")
+    public void testGetUserById() {
+        given()
+                .when()
+                .pathParam("id",1)
+                .get("/posts/{id}")
+                .then()
+                .statusCode(200)
+                .body("userId",equalTo(1))
+                .log();
+        Allure.step("Validación de userId=1 completada");
+    }
+
+        
     }
 }
