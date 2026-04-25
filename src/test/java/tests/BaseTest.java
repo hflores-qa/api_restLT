0a1e55a3fedddff41f51fb93f15d0944eff4a528
hflores-qa <62617562+hflores-qa@users.noreply.github.com>
2026-04-25 10:09:34 -0600

    Test Record

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,14 @@
 package tests;
 
+import io.qameta.allure.junit5.AllureJunit5;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 //import utils.ConfigReader;
+import org.junit.jupiter.api.extension.ExtendWith;
 import utils.ConfigReaderUtils;
 
 public class BaseTest {
-
+    @ExtendWith(AllureJunit5.class)
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = ConfigReaderUtils.get("base.url");
